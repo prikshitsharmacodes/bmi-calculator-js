278214487d17c481f1a762f3c9dbc87fd528a978
Prikshit <prikshitsharma.codes@gmail.com>
2025-10-06 11:36:21 +0530

    Create chaiaurcode.js

---
+++ chaiaurcode.js
@@ -0,0 +1,35 @@
+const form = document.querySelector("form")
+
+form.addEventListener("submit",(e)=>{
+e.preventDefault()
+
+const height = parseInt(document.querySelector("#height").value)
+
+const weight = parseInt(document.querySelector("#weight").value)
+
+const results = document.querySelector("#results")
+if(height === "" || height< 0 || isNaN(height)){
+  results.innerHTML ="Please Give a valid height"
+}
+
+else if(weight === "" || weight< 0 || isNaN(weight)){
+  results.innerHTML ="Please Give a valid weight"
+}
+else{
+ const bmi = (weight / ((height* height)/10000)).toFixed(2)
+
+ if(0<=bmi && bmi <=18.6){
+  results.innerHTML = `<span>${bmi} You are Underweight</span>`
+ }
+ else if (18.6 <= bmi && bmi <=24.9){
+results.innerHTML =`<span> ${bmi} You are in normal range</span>`
+ }
+ else{
+  results.innerHTML = `<span>${bmi} You are fatass</span>`
+ }
+ 
+}
+
+
+
+})
